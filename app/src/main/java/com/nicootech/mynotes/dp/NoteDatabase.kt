677b3cefeafeabf6d84c17b4b539f55a81c1f2d3
Ehsan <ehrath1@gmail.com>
2020-07-06 11:59:06 -0700

    At this branch I made the  Note which is our table and DAO which is Interface with the all fun and Database abstract class includes the methods to build Database and invoking the data also in build.gradle, I add one line for room

---
+++ app/src/main/java/com/nicootech/mynotes/dp/NoteDatabase.kt
@@ -0,0 +1,33 @@
+package com.nicootech.mynotes.dp
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.RoomMasterTable
+
+@Database(
+    entities = [Note::class],
+    version = 1
+)
+abstract class NoteDatabase : RoomDatabase() {
+    abstract fun getNoteDao() :NoteDao
+
+    companion object{
+        private fun buildDatabase(context: Context)= Room.databaseBuilder(
+            context.applicationContext,
+            NoteDatabase::class.java,
+            "notedatabase"
+        ).build()
+
+        @Volatile private var instance : NoteDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context)= instance?: synchronized(LOCK){
+            instance?: buildDatabase(context).also {
+                instance = it
+            }
+
+        }
+    }
+}
